c915c0be8d6ad93c9b40292a64c7d515151a5c9a
MD Al Amin Hasan <alaminhasanpro@gmail.com>
2024-11-24 23:12:11 +0600

    updated

--- src/app/modules/products/product.model.ts
+++ src/app/modules/products/product.model.ts
@@ -5,19 +5,19 @@ const productSchema = new Schema<IProduct>(
   {
     title: { type: String, required: true },
     author: { type: String, required: true },
-    price: { type: Number, required: true, min: 0 },
+    price: { type: Number, required: true },
     category: {
       type: String,
       enum: ["Fiction", "Science", "SelfDevelopment", "Poetry", "Religious"],
       required: true,
     },
     description: { type: String, required: true },
-    quantity: { type: Number, required: true, min: 0 },
+    quantity: { type: Number, required: true },
     inStock: { type: Boolean, default: true },
   },
   {
     timestamps: true,
-  },
+  }
 );
 
 export const Product = model<IProduct>("Product", productSchema);
